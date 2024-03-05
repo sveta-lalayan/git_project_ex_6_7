4e76aacb8b7e03e69c3ff3ad41c38b84fe761534
Sveta Lalayan <lalayan-svetlana@list.ru>
2024-03-05 18:37:23 +0300

    the second commit

--- funcs.py
+++ funcs.py
@@ -6,3 +6,13 @@ num2 = int(input())
 
 result = add_numbers(num1, num2)
 print("Итог:", result)
+
+
+def subtract_numbers(num11, num2):
+    return num1 - num2
+
+num1 = int(input())
+num2 = int(input())
+
+result = subtract_numbers(num1, num2)
+print("Итог:", result)
